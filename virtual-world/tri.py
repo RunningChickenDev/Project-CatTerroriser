72b264f7c4cb8f44df20a1bf9559bf935a00855c
MisterCavespider <cavespider12@gmail.com>
2019-06-30 17:20:30 +0200

    Removed unnecessary whitespaces

--- virtual-world/tri.py
+++ virtual-world/tri.py
@@ -17,7 +17,7 @@ def _sld(a, b, p):
 	         P           ╗
 	         |           ║ _sld(a, b, p) = +1
 	A--------*----->>>B  ╣
-		 |           ║ _sld(a, b, q) = -1
+			 |           ║ _sld(a, b, q) = -1
 	         Q           ╝
 
 	== Example 2 ==
@@ -37,7 +37,7 @@ class ConnEdge:
 	"""
 	A ConnEdge is a half-edge in a polygon
 	with multiple inner faces.
-	
+
 	Fields:
 		origin	-	A vertex from where this line comes (the source)
 		face	-	An identifier for the face to its left
@@ -64,15 +64,15 @@ class DCEL:
 	DCEL stands for Doubly Connected Edge List.
 	Such a list contains a polygon with within
 	chains of vertices.
-	
+
 	The list contains half-edges, represented as ConnEdges.
 	See ConnEdge documentation for more.
-	
+
 	This object also remembers how many faces it handles.
 	This object also remembers the edges.
-	
+
 	Multiple DCELs will have different objects of
-	ConnEdge, as they are different polygons. 
+	ConnEdge, as they are different polygons.
 	"""
 	def __init__(self, vertices, update_vertices = True):
 		self.faces = 1
@@ -83,12 +83,12 @@ class DCEL:
 			e = self.edges[i]
 			e.next = self.edges[ (i+1) % len(self.edges) ]
 			e.prev = self.edges[ (i-1) % len(self.edges) ]
-		
+
 		if update_vertices:
 			for e in self.edges:
 				e.origin.e = e	# Cool syntax bro
 
-	def insert(self, src, tgt):		
+	def insert(self, src, tgt):
 		starters = []			# All edges that start at the source
 		enders = []				# All edges that start at the target
 		for e in self.edges:
@@ -96,7 +96,7 @@ class DCEL:
 				starters += [e]
 			elif e.origin is tgt:
 				enders += [e]
-				
+
 		# If starter and ender dont have the same face,
 		# it means the diagonal cuts through multiple
 		# faces. This is not handled, and returns a ValueError
@@ -109,7 +109,7 @@ class DCEL:
 					face = starter.face
 		if not se_pair:
 			raise ValueError("Cannot find a starter/ender-pair on the same face")
-		
+
 		# Cool syntax bro
 		new_face = self.new_face()
 		left_edge = ConnEdge(src, face, None, se_pair[0].prev, se_pair[1])
@@ -120,7 +120,7 @@ class DCEL:
 		left_edge.prev.next = left_edge
 		left_edge.next.prev = left_edge
 		right_edge.prev.next = right_edge
-		right_edge.next.prev = right_edge		
+		right_edge.next.prev = right_edge
 
 		# Update the new face
 		e = right_edge.next
@@ -142,12 +142,12 @@ class DCEL:
 			if e.twin != None:
 				if not self.Sf.contains(e.twin.face):
 					self._march_face_data(e)
-			
+
 
 	def gen_face_data(self, q=["faces","colours"]):
-		l = logg.get("DCEL")		
+		l = logg.get("DCEL")
 		data = {}
-		
+
 		if "faces" in q:
 			data["network"] = {}
 			polygons = {}
@@ -173,7 +173,7 @@ class DCEL:
 				polygons[e.face] = verts
 				Sf.remove(e.face)
 			data["polys"] = polygons
-		
+
 		if "colours" in q:
 			if "faces" not in q:
 				l.warning("Cannot generate data for DCEL colours if faces are not generated!")
@@ -199,7 +199,7 @@ class Vertex:
 		for t in tuples:
 			r += [Vertex(t[0], t[1])]
 		return r
-	
+
 	def vertices_to_tuples(vertices, whole = False):
 		r = []
 		for v in vertices:
@@ -469,15 +469,15 @@ class TriangleSweep:
 			else:
 				# Clockwise triangle
 				return [[monotone[0], monotone[2], monotone[1]]]
-		
+
 		# init status objects
 		D = []
 		indices = []
-		
+
 		# Up-To-Down Queue
 		q = self._q(monotone)
 		self.l.debug("Monotone priority queue: {}".format(q))
-		
+
 		S = [0,1]
 		for j in range(2,len(monotone)-1):
 			self._l.debug("j = %d", j)
@@ -499,12 +499,12 @@ class TriangleSweep:
 				k = S[-1]
 				self._l.debug("j: %d; k: %d; l: %d", j,k,l)
 				self._l.debug("q[j]: %d; q[k]: %d; q[l]: %d", q[-j-1],q[-k-1],q[-l-1])
-				
+
 				triwind = _sld(self.vertices[q[-j-1]],self.vertices[q[-k-1]],self.vertices[q[-l-1]]) > 0
 				chain = self._chain(monotone, q[-j-1]) > 0
-				
+
 				self._l.debug("Triwind: %d; Chain: %d", triwind, chain)
-				
+
 				while S and triwind == chain:
 					l = k
 					k = S[-1]
@@ -522,7 +522,7 @@ class TriangleSweep:
 		for us in S[1:-1]:
 			D += [Edge(q[-len(monotone)], q[-us-1], None)]
 		self._l.debug("D: %s", D)
-		
+
 		self._l.debug("Indices: %s", indices)
 		return indices
 
@@ -537,7 +537,7 @@ class TriangleSweep:
 		self.D = DCEL(self.vertices)
 		self.queueD = []
 		# TODO: add handling for holes
-		
+
 		for e in self.D.edges:
 			print("E: " + str(e.prev) + " -> " + str(e.origin) + " -> " + str(e.next))
 
@@ -555,7 +555,7 @@ class TriangleSweep:
 		self.l.info("Done partition shape in y-monotone subsets ...")
 		self.l.info("T: {}".format(self.T))
 		self.l.debug("Diagonals to add: {}".format(self.queueD))
-		
+
 		# update D
 		for queuedItem in self.queueD:
 			self.D.insert(queuedItem[0], queuedItem[1])
@@ -607,11 +607,11 @@ if __name__ == '__main__':
 	tuples = [(-1,-2),(0,-1),(1,-2),(2,0),(1,2),(0,1),(-1,2),(-2,0)]
 	vertices = Vertex.tuples_to_vertices(tuples)
 	t = Triangulation(vertices)
-	
+
 #	D = DCEL(vertices)
 #	D.insert(vertices[1], vertices[3])
 #	D.insert(vertices[5], vertices[7])
 #	screen = vis.open_window()
 #	vis.draw_DCEL(screen, D, msg="__main__ test")
-	
+
 	TriangleSweep.triangulate(t)
